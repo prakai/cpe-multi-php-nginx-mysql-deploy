bd92a4ae49d55ea11eba1aa8413ed160b7e670c8
Prakai Nadee <prakai.na@gmail.com>
2026-02-08 11:53:24 +0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,9 @@
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(50) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+INSERT INTO `users` (`username`, `password`, `name`) 
+VALUES ('admin', 'adminpassword', 'Project Administrator');
